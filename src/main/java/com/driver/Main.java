d8aefa97e4b1771e0e8edbcef3c5bd786e641c43
Rajan Chaudhary <Yuvraj518>
2023-04-16 11:52:31 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String args[]){
+        Product p=new Product();
+        p.product(3,4);
+        p.product(3,4,5);
+        p.product(1.2,1);
+    }
+    static class Product{
+        public int product(int x,int y){
+            System.out.println(x*y);
+            return (x*y);
+        }
+        public int product(int x,int y,int z){
+            System.out.println(x*y*z);
+            return(x*y*z);
+        }
+        public double product(double x, double y){
+            System.out.println(x*y);
+            return (x*y);
+        }
+    }
 }
